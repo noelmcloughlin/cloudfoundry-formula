42a16799741ee41700a43ea3209c563c6e376efd
N <noel.mcloughlin@gmail.com>
2019-06-03 10:42:39 +0100

    chore(unittests): basic unit tests for cf-cli

---
+++ test/integration/cli/controls/config_spec.rb
@@ -0,0 +1,10 @@
+control 'cloudfoundry cli' do
+  title 'should match desired lines'
+
+  describe file('/usr/local/bin/cf') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    its('mode') { should cmp '0644' }
+  end
+end
